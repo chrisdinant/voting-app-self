30c34733f45df2a1666e9c3216689a390d81451c
chrisdinant <chris.dinant@gmail.com>
2016-09-20 16:32:08 +0000

    navbar login markup

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,10 @@ var path = process.cwd();
 module.exports = function(app){
     
     app.get('/', function(req, res){
-        res.sendFile(path + '/views/main.html');
+        res.sendFile(path + '/public/index.html');
     });
     
+    app.get('/login', function(req,res){
+        res.sendFile(path + '/public/login.html');
+    });
 };
--- server.js
+++ server.js
@@ -2,6 +2,14 @@ var express = require('express');
 var path = process.cwd();
 var routes = require(path + '/routes/routes.js');
 var app = express();
+var mongoose = require('mongoose');
+var passport = require('passport');
+var session = require('express-session');
+
+require('dotenv').load();
+
+app.use('/public', express.static(path + '/public'));
+app.use("/public/styles", express.static(path + "/public/styles"));
 
 routes(app);
 
